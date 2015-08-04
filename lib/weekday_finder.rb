d9f7af50a103800c5fa81c5da46c4c17571c6bcf
Janice Laset-Parkerson & Tyler Finley <student@epicodus.com>
2015-08-04 10:35:18 -0700

    changed if statements to check  for weekends to use time method

--- lib/weekday_finder.rb
+++ lib/weekday_finder.rb
@@ -3,7 +3,7 @@ class String
     input = self.split('/')
     time = Time.new(input[2], input[0], input[1])
     day_of_week = time.strftime('%A')
-    if day_of_week == 'Saturday' || day_of_week == 'Sunday'
+    if time.saturday?() || time.sunday?()
       message =  day_of_week + ' Sleep in'
     else
       message = day_of_week + ' Get up!'
