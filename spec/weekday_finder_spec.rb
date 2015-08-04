127da8d125e555b2090336d55c198bd4efbc032a
Janice Laset-Parkerson & Tyler Finley <student@epicodus.com>
2015-08-04 10:32:17 -0700

    updated spec to specify weekend days

--- spec/weekday_finder_spec.rb
+++ spec/weekday_finder_spec.rb
@@ -2,12 +2,14 @@ require('rspec')
 require('weekday_finder')
 
 describe('Time#weekday_finder') do
-  it('identifies the current day') do
+  it('identifies the current day and tell user to sleep in if Saturday') do
     expect(('8/15/2015').weekday_finder()).to(eq('Saturday Sleep in'))
   end
-  it('identifies the current day') do
+
+  it('identifies the current day and tell user to sleep in if Sunday') do
     expect(('8/16/2015').weekday_finder()).to(eq('Sunday Sleep in'))
   end
+
   it('identify if weekday and tell user to get up') do
     expect(('8/14/2015').weekday_finder()).to(eq('Friday Get up!'))
   end
