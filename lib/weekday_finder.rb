a9a4ffdb7ffac3c4ea5457cdbfc7ac6a14d90335
Janice Laset-Parkerson & Tyler Finley <student@epicodus.com>
2015-08-04 10:29:24 -0700

    changed original spec to identify day and tell user to sleep in on saturday or sunday tell user to get up on weekday

--- lib/weekday_finder.rb
+++ lib/weekday_finder.rb
@@ -3,5 +3,10 @@ class String
     input = self.split('/')
     time = Time.new(input[2], input[0], input[1])
     day_of_week = time.strftime('%A')
+    if day_of_week == 'Saturday' || day_of_week == 'Sunday'
+      message =  day_of_week + ' Sleep in'
+    else
+      message = day_of_week + ' Get up!'
+    end
   end
 end
--- spec/weekday_finder_spec.rb
+++ spec/weekday_finder_spec.rb
@@ -2,7 +2,13 @@ require('rspec')
 require('weekday_finder')
 
 describe('Time#weekday_finder') do
-  it('identifies the current weekday') do
-    expect(('8/15/2015').weekday_finder()).to(eq('Saturday'))
+  it('identifies the current day') do
+    expect(('8/15/2015').weekday_finder()).to(eq('Saturday Sleep in'))
+  end
+  it('identifies the current day') do
+    expect(('8/16/2015').weekday_finder()).to(eq('Sunday Sleep in'))
+  end
+  it('identify if weekday and tell user to get up') do
+    expect(('8/14/2015').weekday_finder()).to(eq('Friday Get up!'))
   end
 end
